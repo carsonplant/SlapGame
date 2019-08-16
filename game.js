3d3b2188b31090033989b15908b684cd678dfd55
Carson Plant <carsonplant22@gmail.com>
2019-08-16 15:27:21 -0600

    3 buttons

--- game.js
+++ game.js
@@ -5,7 +5,18 @@ function slap() {
   health--
   alert(health)
 }
+function punch() {
+  health -= 5
+  alert(health)
+}
+function kick() {
+  health -= 10
+  alert(health)
+}
 
+function update() {
+  document.getElementById("health").innerText;
 
+}
 
-//slap()
+update()
