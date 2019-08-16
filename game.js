1f3171e7a072958feacc7d6e083821b79e8cdaad
Carson Plant <carsonplant22@gmail.com>
2019-08-16 15:59:38 -0600

    heading tags still no values :)

--- game.js
+++ game.js
@@ -1,21 +1,31 @@
 let health = 100;
+let targetName = "Fighting Chicken";
+let targetHits = 0;
 
+function hitCounter() {
+  targetHits++
+}
 
 function slap() {
   health--
   alert(health)
+  update()
+
 }
 function punch() {
   health -= 5
   alert(health)
+  update()
 }
 function kick() {
   health -= 10
   alert(health)
+  update()
 }
 
 function update() {
-  document.getElementById("health").innerText;
+  document.getElementById("health").innerText //+= health
+  document.getElementById("name").innerText //+= targetName
 
 }
 
