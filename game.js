fe8138d07330572e4d225f5053fcb599f6545757
Carson Plant <carsonplant22@gmail.com>
2019-08-17 15:20:20 -0600

    items buttons beginning

--- game.js
+++ game.js
@@ -1,31 +1,82 @@
-let health = 100;
-let targetName = "Fighting Chicken";
-let targetHits = 0;
-
-function hitCounter() {
-  targetHits++
+let chicken = {
+  health: 100,
+  name: "Fighting Chicken",
+  targetHits: 0,
+  items: []
 }
 
+let items = {
+
+  pan: {
+    name: "Frying Pan",
+    modifier: 5
+  },
+
+  bat: {
+    name: "Baseball bat",
+    modifier: 10,
+  },
+
+  helmet: {
+    name: "Helmet",
+    modifier: -10
+  }
+
+}
+let modActive = false
+
 function slap() {
-  health--
-  alert(health)
+  if (chicken.health >= 1) {
+
+    let damage = 1 + addMods();
+    chicken.health -= damage;
+    chicken.targetHits++
+
+  }
   update()
 
 }
 function punch() {
-  health -= 5
-  alert(health)
+  if (chicken.health >= 1) {
+    let damage = 5
+    chicken.health -= damage;
+    chicken.targetHits++
+  }
   update()
 }
 function kick() {
-  health -= 10
-  alert(health)
+  if (chicken.health >= 1) {
+    let damage = 10
+    chicken.health -= damage
+    chicken.targetHits++
+  }
   update()
 }
 
+function usePan() {
+  if (modActive == false) {
+    chicken.items.push(items.pan)
+    modActive = true
+  }
+  update()
+}
+function addMods() {
+  debugger
+  var mods = 0
+  for (let i = 0; i <= chicken.items.length; i++) {
+    mods = chicken.items[i]
+  }
+  return mods
+}
 function update() {
-  document.getElementById("health").innerText //+= health
-  document.getElementById("name").innerText //+= targetName
+  document.getElementById("name").innerText = `${chicken.name}` //+= targetName
+  document.getElementById("hits").innerText = `${chicken.targetHits}` //+= targetName
+  if (chicken.health <= 0) {
+    document.getElementById("health").innerText = "Defeated" //+= health
+
+  } else {
+    document.getElementById("health").innerText = `${chicken.health}` //+= health
+  }
 
 }
 
